60ddc0e7a3f1b52027bb4000dde567b60a8b1403
higorasilverio <higorasilverio@outlook.com>
2020-04-10 12:56:58 -0300

    Game duration calcule by the inicial and final hour - Final

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("*** Game duration calcule by the inicial and final hour ***");
+		
+		System.out.print("Enter the inicial hour: ");
+		int initial = scanner.nextInt();
+		System.out.print("Enter the final hour: ");
+		int end = scanner.nextInt();
+		int duration = end - initial;
+		
+		if (duration >= 0) {
+			System.out.println("Game duration = " + duration + " hour(s)!");
+		}
+		else {
+			duration += 24;
+			System.out.println("Game duration = " + duration + " hour(s)!");
+		}
+		
+		scanner.close();		
+	}
+
+}
